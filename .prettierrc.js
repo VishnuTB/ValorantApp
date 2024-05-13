1bc0023d98b2af0584993167a00dce4bc2522bb1
Vishnu T B <vishnutb123@gmail.com>
2024-05-13 11:29:48 +0530

    Prehook

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,14 @@
+// module.exports = {
+//   arrowParens: 'avoid',
+//   bracketSameLine: true,
+//   bracketSpacing: false,
+//   singleQuote: true,
+//   trailingComma: 'all',
+// };
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  semi: true,
+  trailingComma: "none",
+  singleQuote: false,
+  printWidth: 80,
+  useTabs: true
 };
